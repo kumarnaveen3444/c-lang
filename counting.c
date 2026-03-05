3157620913f966bd2e9355e48a33de534239039a
kumar-naveen <naveenyadav34444@gmail.com>
2026-03-05 06:17:20 +0530

    count

---
+++ counting.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+int d;
+ printf("Enter a integer: ");
+
+    scanf("%d", &d);
+
+    if ((d >= 1 && d <= d) || (d >= 1 && d<= d))
+
+        printf("%d is an counting", d);
+
+    else
+
+        printf("%d is not an counting", d);
+
+
+    return 0;
+}
+
+
+	
