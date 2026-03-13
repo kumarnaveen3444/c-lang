ecd8c3d6e4809b8684eb1403c7000f7098c1a71d
kumar-naveen <naveenyadav34444@gmail.com>
2026-03-13 23:27:39 +0530

    to swap to numbers

---
+++ swap_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+
+  int = a , b;
+
+  printf("Enter a: ");
+  scanf("%d", &a);
+
+  printf("Enter b: ");
+  scanf("%d", &b);
+
+  a = a - b;   
+
+
+  a = b - a;
+
+  printf("After swapping, a = %.21d\n", a);
+  printf("After swapping, b = %.21d", b);
+
+  return 0;
+}
