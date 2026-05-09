e356fbc0f31ee62f10ea0a2dfcb62066adaa2320
kumar-naveen <naveenyadav34444@gmail.com>
2026-05-09 17:42:20 +0530

    operators on integers

---
+++ operators.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+
+        int x;
+
+        x = 7;
+
+        printf("\n%d",x);
+
+        x=x+7;
+
+        printf("\n%d",x);
+
+        x=x*2;
+
+        printf("\n%d",x);
+
+        x=x/2;
+
+        printf("\n%d",x);
+
+        x=x-4;
+
+        printf("\n%d",x);
+
+    return 0;
+}
