4d7c6d75fe268697cc7a08dea32cefe34fa4212c
kumar-naveen <naveenyadav34444@gmail.com>
2026-06-02 21:19:56 +0530

    to find odd and even values

---
+++ alphabet_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+int main() {
+    char ch;
+    int position;
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch);
+
+    if (isalpha(ch)) {
+    
+        char upperCh = toupper(ch);
+        
+    
+        position = upperCh - 'A' + 1;
+
+        printf("The position of '%c' in the alphabet is: %d\n", ch, position);
+    } else {
+        printf("Error: '%c' is not an alphabet.\n", ch);
+    }
+
+    return 0;
+}
