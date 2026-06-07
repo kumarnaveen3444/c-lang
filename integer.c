83702cceae276513eba392eba6c1e50f8238f254
kumar-naveen <naveenyadav34444@gmail.com>
2026-06-07 21:36:27 +0530

    integer

---
+++ integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {   
+    int num;
+   
+    printf("Enter an integer: ");  
+    
+    scanf("%d", &num);
+
+    printf(" %d entered: ", num);
+    
+    return 0;
+}
