8b8dce8f0336b95d6b02c01c3147509baaae43ad
kumar-naveen <naveenyadav34444@gmail.com>
2026-06-19 14:14:14 +0530

    to find the lcm of two numbers

---
+++ lcm.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    int max;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2); 
+    
+    max = (num1 > num2) ? num1 : num2;
+    
+    while(1) {
+        if(max % num1 == 0 && max % num2 == 0)
+        {
+            printf(" LCM of %d and %d is %d\n", num1, num2, max);
+            break;
+        }
+        max++;
+    }
+    
+    return 0;
+}
